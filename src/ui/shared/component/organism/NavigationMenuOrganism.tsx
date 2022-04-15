ee75935bdddc0d1fe2964fe92b7aa72dc2240196
Kevin Cabrera <kcabrera@spalopia.com>
2022-04-15 22:15:18 +0100

    Deleted fontawesome import

--- src/ui/shared/component/organism/NavigationMenuOrganism.tsx
+++ src/ui/shared/component/organism/NavigationMenuOrganism.tsx
@@ -1,4 +1,3 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
 import { useLocation } from "react-router-dom";
 import { ButtonAtom } from "../atom/button/ButtonAtom";
